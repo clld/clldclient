72a31756a906ade97946fce83a5407a01d02d642
xrotwang <xrotwang@googlemail.com>
2015-09-22 15:47:08 +0200

    fleshed out concepticon client

--- clldclient/concepticon.py
+++ clldclient/concepticon.py
@@ -13,8 +13,25 @@ class Concept(Resource):
         return vs._get_first_resource('dcterms:isPartOf')
 
 
+class ConceptSet(Resource):
+    @property
+    def description(self):
+        return self.get_text('dcterms:description')
+
+    @property
+    def ontological_category(self):
+        return self.get_text('dcterms:type')
+
+    @property
+    def alt_labels(self):
+        for uriref in self['dcterms:hasPart']:
+            vs = self.client.resource(uriref)
+            yield vs.get_text('dcterms:description', language='en')
+
+
 class Concepticon(Database):
     def __init__(self):
+        self.__resource_map__['parameter'] = ConceptSet
         self.__resource_map__['value'] = Concept
         Database.__init__(self, 'concepticon.clld.org')
 
